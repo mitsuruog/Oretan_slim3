c086dba8bd9d28de468b3df56625b05593ba2486
Mitsuru Ogawa <mitsuru.ogawa.jp@gmail.com>
2012-11-02 23:05:51 +0900

    implimented server side validation

--- src/com/mitsuruog/gae/oretan/controller/word/PutController.java
+++ src/com/mitsuruog/gae/oretan/controller/word/PutController.java
@@ -4,8 +4,11 @@ import net.arnx.jsonic.JSON;
 
 import org.slim3.controller.Controller;
 import org.slim3.controller.Navigation;
+import org.slim3.controller.validator.Errors;
+import org.slim3.controller.validator.Validators;
 import org.slim3.util.RequestMap;
 
+import com.mitsuruog.gae.oretan.meta.WordMeta;
 import com.mitsuruog.gae.oretan.model.Word;
 import com.mitsuruog.gae.oretan.service.WordService;
 import com.mitsuruog.gae.util.Auth;
@@ -14,12 +17,23 @@ import com.mitsuruog.gae.util.Resp;
 public class PutController extends Controller {
 
     private WordService service = new WordService();
+    private WordMeta m = WordMeta.get();
 
     @Override
     public Navigation run() throws Exception {
 
         if(Auth.isAuthenticated(request) == false) {
             return Resp.redirectLoginForm(request, response);
+        }
+
+        Validators v = new Validators(request);
+        v.add(m.en, v.required(), v.maxlength(140));
+        v.add(m.ja, v.required(), v.maxlength(140));
+        if(!v.validate()){
+            Errors errors = v.getErrors();
+            String json = JSON.encode(errors);
+            Resp.responseErrorJson(response, json);
+            return null;
         }
 
         Word word = service.add(new RequestMap(request));
--- src/com/mitsuruog/gae/util/Resp.java
+++ src/com/mitsuruog/gae/util/Resp.java
@@ -14,58 +14,75 @@ import com.google.appengine.api.users.UserServiceFactory;
 
 /**
  * Response処理のUtilityクラス
+ *
  * @author mitsuruog
  *
  */
 public class Resp {
 
-	private final static String CONTENT_TYPE_JSON = "application/json";
-	private final static String CONTENT_TYPE_HTML = "text/html";
-	private final static String CHAR_ENCODE_UTF_8 = "UTF-8";
+    private final static String CONTENT_TYPE_JSON = "application/json";
+    private final static String CONTENT_TYPE_HTML = "text/html";
+    private final static String CHAR_ENCODE_UTF_8 = "UTF-8";
 
-	/**
-	 * ログインURLへリダイレクト
-	 * @param req
-	 * @param resp
-	 * @throws IOException
-	 */
-	public static Navigation redirectLoginForm(HttpServletRequest req, HttpServletResponse resp) throws IOException{
-	    UserService service = UserServiceFactory.getUserService();
+    /**
+     * ログインURLへリダイレクト
+     *
+     * @param req
+     * @param resp
+     * @throws IOException
+     */
+    public static Navigation redirectLoginForm(HttpServletRequest req,
+            HttpServletResponse resp) throws IOException {
+        UserService service = UserServiceFactory.getUserService();
         return new Navigation(service.createLoginURL(req.getRequestURI()), true);
-	}
+    }
 
-	/**
-	 * JSONでResponseを返す
-	 * @param resp
-	 * @param json
-	 * @throws IOException
-	 */
-	public static void responseJson(HttpServletResponse resp, String json) throws IOException{
-		resp.setContentType(CONTENT_TYPE_JSON);
-		resp.setCharacterEncoding(CHAR_ENCODE_UTF_8);
-		resp.setStatus(SC_OK);
-		resp.getWriter().print(json);
-	}
+    /**
+     * JSONでResponseを返す
+     *
+     * @param resp
+     * @param json
+     * @throws IOException
+     */
+    public static void responseJson(HttpServletResponse resp, String json)
+            throws IOException {
+        resp.setContentType(CONTENT_TYPE_JSON);
+        resp.setCharacterEncoding(CHAR_ENCODE_UTF_8);
+        resp.setStatus(SC_OK);
+        resp.getWriter().print(json);
+    }
 
-	/**
-	 * text/htmlでReaponseを返す
-	 * @param resp
-	 * @throws IOException
-	 */
-	public static void responseHtml(HttpServletResponse resp) throws IOException{
-		resp.setContentType(CONTENT_TYPE_HTML);
-		resp.setCharacterEncoding(CHAR_ENCODE_UTF_8);
-		resp.setStatus(SC_OK);
-	}
+    /**
+     * text/htmlでReaponseを返す
+     *
+     * @param resp
+     * @throws IOException
+     */
+    public static void responseHtml(HttpServletResponse resp)
+            throws IOException {
+        resp.setContentType(CONTENT_TYPE_HTML);
+        resp.setCharacterEncoding(CHAR_ENCODE_UTF_8);
+        resp.setStatus(SC_OK);
+    }
 
-	/**
-	 * エラーページへ遷移
-	 * @param resp
-	 * @param message
-	 * @throws IOException
-	 */
-	public static void responseError(HttpServletResponse resp, String message) throws IOException {
-		resp.sendError(SC_INTERNAL_SERVER_ERROR);
-	}
+    /**
+     * エラーページへ遷移
+     *
+     * @param resp
+     * @param message
+     * @throws IOException
+     */
+    public static void responseError(HttpServletResponse resp, String message)
+            throws IOException {
+        resp.sendError(SC_INTERNAL_SERVER_ERROR);
+    }
+
+    public static void responseErrorJson(HttpServletResponse resp, String json)
+            throws IOException {
+        resp.setContentType(CONTENT_TYPE_JSON);
+        resp.setCharacterEncoding(CHAR_ENCODE_UTF_8);
+        resp.setStatus(SC_BAD_REQUEST);
+        resp.getWriter().print(json);
+    }
 
 }
